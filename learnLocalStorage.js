a03c831a4d3d909f4512883544ffe73c045683f3
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2021-10-18 17:54:42 -0400

    18-10

--- learnLocalStorage.js
+++ learnLocalStorage.js
@@ -14,12 +14,54 @@
 
 
 // To Store The Data->
-var value = "sid";
-localStorage.setItem("name", "siddesh");
-localStorage.setItem("name2", value);
+    // var value = "sid";
+    // localStorage.setItem("name", "siddesh");
+    // localStorage.setItem("name2", value);
 
 
 
 // To Get The Data In Console->
-var myData = localStorage.getItem("name2");
- console.log(myData);
+    // var myData = localStorage.getItem("name2");
+    // console.log(myData);
+
+
+
+ //  To Remove An Item ->
+    // localStorage.removeItem("name");
+    // localStorage.removeItem("name2");
+
+
+
+// To Clear LocalStorage ->
+
+    // localStorage.clear();
+
+// getItem;
+// setItem;
+// removeItem
+// clear
+
+
+
+// Array In LocalStorage->
+    // var arr = [1, 2, 3, 4];
+
+    // localStorage.setItem("myArr", arr);
+
+    // var myData = localStorage.getItem("myArr");
+    // console.log(myData);
+
+
+
+     
+var div = document.getElementById("div");
+// Object In LocalStorage->
+
+    var obj = {name:'siddesh'};
+
+    localStorage.setItem("myObj", obj);
+
+    var myData = localStorage.getItem("myObj");
+    console.log(myData);
+
+    div.append(myData);
