a7a0273465604317df91b8f5f8bdcf10136e2790
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2021-10-31 13:13:31 -0400

    31-10

---
+++ import Export/components/navBar.js
@@ -0,0 +1,18 @@
+function navBar(){
+
+    return `<div id="navBar">
+
+        <h3> <a href="/"> Home </a> </h3>
+
+        <div id="options">
+            <h3> <a href="jewellery.html"> Jewellery </a> </h3>
+            <h3> <a href="electronics.html"> Electronics </a> </h3>
+            <h3> <a href="#"> Sign In </a> </h3>
+            <h3> <a href="#"> Sign Up</a> </h3>
+
+        </div>
+
+    </div>`
+}
+
+export default navBar
