a39793f3352da515e98812f4c3a76d6aa45c8dea
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2021-10-31 18:34:59 -0400

    31-10

---
+++ import Export/scripts/showData.js
@@ -0,0 +1,32 @@
+//getData()
+//appendData()
+
+
+async function getData(url){
+    let res = await fetch(url)
+
+    let data = await res.json();
+    return data
+}
+
+function appendData(data, container){
+
+    data.forEach(({title,image}) =>{
+
+        let Div = document.createElement("div");
+        let p = document.createElement("p");
+
+        p.innerText =  title
+
+        let img = document.createElement("img");
+        img.src = image
+        
+        Div.append(img,p)
+
+        container.append(Div);
+
+
+    })
+}
+
+export {getData, appendData}
